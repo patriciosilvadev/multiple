cd513b4430ca8f85ca116d268bb4d029bf6a1b3d
wahyuutomoputra <wahyuutomoputra@gmail.com>
2020-12-17 17:42:09 +0700

    static file

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('./app/config/socket.config')(server);
+const path = require('path');
+
+app.use(require('express').static('client/build'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
 
 require('./app/config/autoload')(app, io);
 server.listen(8080, () => console.log(`listening port ${8080}`));
