4b71188828a4b34af5c671625caa9b6496795282
wahyuutomoputra <wahyuutomoputra@gmail.com>
2020-12-17 18:42:00 +0700

    cors

--- app/config/autoload.js
+++ app/config/autoload.js
@@ -1,4 +1,7 @@
 module.exports = (app, io = null) => {
+    const cors = require('cors')
+
+    app.use(cors());
     require('../middleware/basic')(app);
     //require("../models");
     require('../routes')(app, io);
